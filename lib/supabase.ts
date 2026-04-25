8f991b0f7b79ab687e819b600bcc1097639622d2
Steve Nathan de Sa <stevenathandesa@gmail.com>
2026-04-25 13:39:25 -0600

    change

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,7 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseKey  = process.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseKey  = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 export const hasSupabaseConfig = Boolean(supabaseUrl && supabaseKey);
 
